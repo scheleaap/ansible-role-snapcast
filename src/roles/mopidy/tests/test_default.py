3210d011324fe26065cac9b9668c6779ed3520f0
scheleaap <git@wout.maaskant.info>
2017-04-25 22:23:22 +0200

    feat: Install mopidy-spotify, configure HTTP

--- src/roles/mopidy/tests/test_default.py
+++ src/roles/mopidy/tests/test_default.py
@@ -4,11 +4,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-# def test_mopidy_apt_repository_present(File):
-#     f = File("/etc/apt/sources.list.d/mopidy.list")
-#     assert f.exists
-#     content = f.content
-#     assert "http://apt.mopidy.com/" in content
+def test_mopidy_apt_repository_present(File):
+    f = File("/etc/apt/sources.list.d/mopidy.list")
+    assert f.exists
+    content = f.content
+    assert "http://apt.mopidy.com/" in content
 
 
 def test_mopidy_is_installed(Package):
@@ -19,17 +19,20 @@ def test_mopidy_is_installed(Package):
 def test_mopidy_configuration(File):
     f = File("/etc/mopidy/mopidy.conf")
     assert f.exists
-    assert f.user == 'mopidy'
+    # TODO: See if we can access the mopidy_user variable here
+    assert f.user == 'root'
     content = f.content
     assert "[spotify]" in content
+    assert "port = 6680" in content
+    assert "hostname = ::" in content
+
+
+def test_mopidy_spotify_is_installed(Package):
+    package = Package("mopidy-spotify")
+    assert package.is_installed
 
 
 # def test_nginx_running_and_enabled(Service):
 #     nginx = Service("nginx")
 #     assert nginx.is_running
 #     assert nginx.is_enabled
-
-
-def test_mopidy_spotify_is_installed(Package):
-    package = Package("mopidy-spotify")
-    assert package.is_installed
